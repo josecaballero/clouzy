c0f9b752d1ac91aeeb68983075fa5d064400c168
josecaballero <jlcab8@gmail.com>
2022-04-03 21:33:01 +0200

    se añade pagina shop, componente preview y component item + estilos.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 import Homepage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => {
-  return <div>Hats Page</div>;
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/hats" component={HatsPage} />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
+//React17
+/* ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById("root")
+); */
+
+//React18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
