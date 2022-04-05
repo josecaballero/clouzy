69c0e5b39f1e8b82bc33fa9c71929896b2dc543c
josecaballero <jlcab8@gmail.com>
2022-04-05 19:23:43 +0200

    Terminando Sign in handleSubmit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,31 +18,6 @@ const config = {
   appId: "1:450008457117:web:08dfaad257fe7b7e3a1974",
 };
 
-/* export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
-
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
-
-  const snapShot = await userRef.get();
-
-  if (!snapShot.exist) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
-
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData,
-      });
-    } catch (error) {
-      console.log("error creating user", error.message);
-    }
-  }
-  return userRef;
-}; */
-
 firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
