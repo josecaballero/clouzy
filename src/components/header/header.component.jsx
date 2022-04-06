97cdd2a9d8973b2f9fb9c3e5387d04cbe0e8c60a
josecaballero <jlcab8@gmail.com>
2022-04-06 11:59:54 +0200

    se añade cart-dropdown y cart-reducer para mostrar y ocultar cuando se hace click en el cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CartIcon from "./../cart-icon/cart-icon.component";
+import CartDropdown from "./../cart-dropdown/cart-dropdown.component";
 
 import { connect } from "react-redux";
 
@@ -10,7 +11,7 @@ import { ReactComponent as Logo } from "../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -33,11 +34,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
