4d405b2bdc2af2c88e3a7483e9fb2667e2d68621
josecaballero <jlcab8@gmail.com>
2022-04-06 10:29:20 +0200

    se añade redux y se implementan user reducer y user actions al header y app components añade redux y se implementan user reducer y user actions al header y app components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
+
+import store from "./redux/store";
 
 //React17
 /* ReactDOM.render(
@@ -16,7 +19,9 @@ import App from "./App";
 //React18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
