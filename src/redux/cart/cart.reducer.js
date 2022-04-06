0f7e41706f3498ac869bb6ffc86f43549491abd5
josecaballero <jlcab8@gmail.com>
2022-04-06 18:50:59 +0200

    se actualiza shop-data.js y se añade cart utils para agrupar items en cart reducer

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
 import CartActionTypes from "./cart.types";
+import { addItemtoCart } from "./cart.utils";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,6 +11,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         hidden: !state.hidden, //Se muestra si está oculta y viceversa
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemtoCart(state.cartItems, action.payload),
       };
     default:
       return state;
