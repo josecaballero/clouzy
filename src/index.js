c1f41a41965604c93180c2cabf33df517234db54
josecaballero <jlcab8@gmail.com>
2022-04-11 18:35:02 +0200

    se mueven los datos de directory y shop a su propio reducer. Se crea el routing a partir de directory/shop

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
-
-import store from "./redux/store";
 
 //React17
 /* ReactDOM.render(
@@ -21,7 +22,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>
 );
