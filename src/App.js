ef31e8cad181001a2432fb59e24202f047de575a
josecaballero <jlcab8@gmail.com>
2022-04-11 22:42:10 +0200

    delete test

--- src/App.js
+++ src/App.js
@@ -77,20 +77,3 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-
-$ git filter-branch --env-filter '
-OLD_EMAIL="jcaballero@takeaway.es"
-NEW_NAME="Jose"
-NEW_EMAIL="jlcab8@gmail.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-  export GIT_COMMITTER_NAME="$NEW_NAME"
-  export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-  export GIT_AUTHOR_NAME="$NEW_NAME"
-  export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags
